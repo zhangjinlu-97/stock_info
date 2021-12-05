eefb9940ea06a6bcf63ace8eb8e7276fe0e032b2
zhangjinlu.97 <zjl_zhx@163.com>
2021-12-05 19:31:31 +0800

    update

--- dao/session.py
+++ dao/session.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # 初始化数据库连接:
-engine = create_engine('mysql+mysqlconnector://username:password@rm-bp1k3z55sf0g51317bo.mysql.rds'
-                       '.aliyuncs.com:3306/db_stock_info', pool_size=20, max_overflow=0)
+engine = create_engine('', pool_size=20, max_overflow=0)
 # 创建DBSession类型:
 DBSession = sessionmaker(bind=engine)
