b93b40c849184dcebadad603080486659fd9cd37
zhangjinlu.97 <zjl_zhx@163.com>
2021-12-06 11:48:28 +0800

    remove mysql url

--- dao/stock_info_dao.py
+++ dao/stock_info_dao.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from entity import StockInfo
 
-DB_URL = 'mysql+mysqlconnector://top18795876066:Zjl20151342019@rm-bp1k3z55sf0g51317bo.mysql.rds.aliyuncs.com:3306/db_stock_info'
+DB_URL = 'mysql+mysqlconnector://username:password@127.0.0.1:3306/db_stock_info'
 
 
 class StockInfoDao(object):
