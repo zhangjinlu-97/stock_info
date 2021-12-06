420be308e797de60682904d221e8ae0d7d3be97d
zhangjinlu.97 <zjl_zhx@163.com>
2021-12-06 11:44:59 +0800

    add update cronjob

--- service/update_data_cronjob.py
+++ service/update_data_cronjob.py
@@ -10,15 +10,3 @@ def update_data_every_day(stock_info_service: StockInfoService):
         stock_info_service.update_stock_infos()
         print(time.asctime(time.localtime(time.time())), ': stock data updated')
         time.sleep(3600 * 24)  # 每天更新一次
-
-
-def test_thread():
-    while True:
-        time.sleep(1)
-        logging.warning('start update data')
-
-
-if __name__ == '__main__':
-    thread.start_new_thread(test_thread, ())
-    print("---------")
-    time.sleep(10)
